86b0595318c6e2022cefe215bbe3641f175f1cc8
DylanCheetah <dylan_the_cheetah@outlook.com>
2023-12-01 17:14:55 -0500

    Removed unnecessary code.

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,9 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext} from 'react';
 import {AppContext} from '../context/AppContext';
 
 
 const Budget = () => {
     const {dispatch, budget} = useContext(AppContext);
-    //const [newBudget, setNewBudget] = useState(budget);
 
     const handleBudgetChange = (event) => {
         //setNewBudget(event.target.value);
